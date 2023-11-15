439377f805d0402f2af742e6ae948bdfb18b6ec9
eddiek001 <worksofeddy@gmail.com>
2023-11-15 17:42:00 +0300

    strlen

--- eddiestrlen.c
+++ eddiestrlen.c
@@ -1,5 +1,12 @@
 #include "eddieshell.h"
 
+/**
+ * eddie_strlen - function that retuns length of the str
+ *
+ * @eddiestr: - input string
+ *
+ * Return: 1 if success
+ */
 size_t eddie_strlen(const char *eddiestr)
 {
 	unsigned int l = 0;

80b145ff996459829322aea7b4219bb442f7ed0c
annmulwa <annmulwa2000@gmail.com>
2023-11-16 03:35:01 -0800

    simple shell

--- annexecution.c
+++ annexecution.c
@@ -33,9 +33,25 @@ void eddie_execution(char *eddiecommand)
 	if (eddiecmd_pathfile == NULL)
 	{
 		ann_printf("NO COMMAND FOUND\n");
-		return;
 	}
-	anncreate_chp(argv);
-
-	free(eddiecmd_pathfile);
+	else
+	{
+		pid_t ch_p = fork();/*creation of the child process*/
+		if (ch_p == -1)/*if the child process has an error*/
+		{
+			perror("eddiefork");
+			exit(EXIT_FAILURE);
+		}
+		if (ch_p == 0)/*child process created successfully*/
+		{
+			execve(argv[0], argv, NULL);
+			perror("annexec");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);/*parent process*/
+			free(eddiecmd_pathfile);
+		}
+	}
 }
